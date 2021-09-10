4a113bf8b18e558ce00a8f56045dd97e3acc185c
G. Michael Fortin <myk410@gmail.com>
2021-09-10 10:45:34 -0400

    Initial Commit

---
+++ BlueTwoth/BlueTwothApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueTwothApp.swift
+//  BlueTwoth
+//
+//  Created by G. Michael Fortin Jr on 9/10/21.
+//
+
+import SwiftUI
+
+@main
+struct BlueTwothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlueTwoth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BlueTwoth
+//
+//  Created by G. Michael Fortin Jr on 9/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
